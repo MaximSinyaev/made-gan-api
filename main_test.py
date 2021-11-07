62049ba06c02a484afb3242f22cea26660c23b49
Maxim Sinyaev <Maxim.Sinyaev@ihsmarkit.com>
2021-11-07 19:29:12 +0300

    bug fixing

--- main_test.py
+++ main_test.py
@@ -3,5 +3,5 @@ from app.image_generation.image_generator import ImageGeneratorGAN
 
 if __name__ == "__main__":
     gan = ImageGeneratorGAN(device="cuda:3")
-    gan.generate_picture("Squirel family")
+    gan.generate_picture("Squirel family", width=100, height=100)
 
