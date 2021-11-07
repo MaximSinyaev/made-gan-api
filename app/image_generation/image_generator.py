955a2d31e11699822a3cde2eed9188f44011db3d
Maxim Sinyaev <Maxim.Sinyaev@ihsmarkit.com>
2021-11-07 19:10:21 +0300

    bug fixing

--- app/image_generation/image_generator.py
+++ app/image_generation/image_generator.py
@@ -39,7 +39,7 @@ class ImageGeneratorGAN:
     def __init__(
         self, model_name="vqgan_imagenet_f16_16384", seed=-1, device=None,
     ) -> None:
-        self.base_model = model_names[model_name]
+        self.base_model = self.model_names[model_name]
         if device is None:
             self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         else:
