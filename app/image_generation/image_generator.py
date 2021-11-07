c2d4de4f1a0a030d208cfaf6067ab84dbe3e06f4
Maxim Sinyaev <Maxim.Sinyaev@ihsmarkit.com>
2021-11-07 21:45:32 +0300

    bug fixing

--- app/image_generation/image_generator.py
+++ app/image_generation/image_generator.py
@@ -125,7 +125,7 @@ class ImageGeneratorGAN:
         z.requires_grad_(True)
         opt = optim.Adam([z], lr=args.step_size)
 
-        normalize = transforms.Normalize(
+        self.normalize = transforms.Normalize(
             mean=[0.48145466, 0.4578275, 0.40821073],
             std=[0.26862954, 0.26130258, 0.27577711],
         )
