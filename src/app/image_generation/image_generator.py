97b355deb28a1de663e4040039a6acefe7cfaa6d
Yaroslav Khnykov <zingykizzz@gmail.com>
2021-11-09 16:18:39 +0300

    изменена задержка инициации

--- src/app/image_generation/image_generator.py
+++ src/app/image_generation/image_generator.py
@@ -3,13 +3,18 @@ from typing import ClassVar
 
 
 class ImageGeneratorGAN:
-    START_DELAY: ClassVar[int] = 1
-    GENERATION_DELAY: ClassVar[int] = 15
+    START_DELAY: ClassVar[int] = 60
+    GENERATION_DELAY: ClassVar[int] = 30
     DEFAULT_RESULT: str = "image.png"
 
     def __init__(self):
-        time.sleep(self.START_DELAY)
+        self.is_ready = False
+        self.start()
 
     def generate_image(self, text: str) -> str:
         time.sleep(self.GENERATION_DELAY)
         return self.DEFAULT_RESULT
+
+    def start(self):
+        time.sleep(self.START_DELAY)
+        self.is_ready = True
