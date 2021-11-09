7d8d3f0fbcc867f2a39cfd1477b48ac604afab2b
Yaroslav Khnykov <zingykizzz@gmail.com>
2021-11-10 02:43:38 +0300

    изменен ответ запроса

--- src/app/main.py
+++ src/app/main.py
@@ -1,12 +1,11 @@
 import os
-from fastapi import FastAPI, Request, BackgroundTasks
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from celery.result import AsyncResult
 from starlette.responses import RedirectResponse
 
 from worker.celery_app import celery_app
-from utils import background_on_message, celery_inspect
 
 templates = Jinja2Templates(directory=os.environ["TEMPLATES_DIRECTORY"])
 
@@ -18,7 +17,7 @@ app.mount(
 
 
 @app.api_route("/generate_image", methods=["GET", "POST"])
-async def generate_image(request: Request, background_task: BackgroundTasks):
+async def generate_image(request: Request):
     text = ""
     if request.method == "POST":
         form = await request.form()
@@ -26,7 +25,6 @@ async def generate_image(request: Request, background_task: BackgroundTasks):
         task = celery_app.send_task(
             os.environ["CELERY_GENERATE_IMAGE_TASK_NAME"], args=[text]
         )
-        background_task.add_task(background_on_message, task)
         response = RedirectResponse(url=f"/tasks/{task.id}")
         response.status_code = 302
         return response
@@ -38,18 +36,8 @@ async def generate_image(request: Request, background_task: BackgroundTasks):
 @app.get("/tasks/{task_id}")
 async def get_result(request: Request, task_id: str):
     res = AsyncResult(task_id)
-    if res.ready():
-        return templates.TemplateResponse(
-            "page.html", {"request": request, "generated_image": res.result},
-        )
-    try:
-        text, start_time, current_time, wasted_seconds = celery_inspect(celery_app, task_id)
-        start_time = start_time.strftime("%H:%M:%S")
-        current_time = current_time.strftime("%H:%M:%S")
-        return (
-            f"Статус: {res.status}, Текст: {text}, Время старта: {start_time}, "
-            f"Текущее время: {current_time}, Потрачено секунд: {wasted_seconds}"
-        )
-    except KeyError:
-        return "Обновите страницу позднее"
-
+    generated_image = res.result if res.ready() else "gen.gif"
+    return templates.TemplateResponse(
+        "page.html",
+        {"request": request, "status": res.status, "generated_image": generated_image},
+    )
